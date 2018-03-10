383773b604f32512386b6939a9953f8779ffa8e3
Aminul Islam <amin13522@gmail.com>
2018-03-10 14:22:45 -0500

    commit

--- src/index.js
+++ src/index.js
@@ -2,13 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
-import CardList from './components/CardList';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
-import { robots } from './robots';
 
-ReactDOM.render(
-	        <App />
-	, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
-
